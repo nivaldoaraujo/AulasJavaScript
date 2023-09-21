91f4c5a861498af2c0aaf0b1b904f48bf005839e
nivaldoaraujo <batista_nba@hotmail.com>
2023-09-21 13:27:45 -0300

    quarto comit

---
+++ AULA08-teste/funcaoanonimas.js
@@ -0,0 +1,22 @@
+// function contar()
+// {
+//     //var c = 0
+//     for(var x = 0; x <= 10 ; x++)
+//     {
+//         console.log(x)
+//     }
+// }
+// contar()
+// console.log("--------------------------------------------")
+// var soma = function (a, b)
+// {
+//     return a + b
+// }
+
+// console.log(soma(2, 2))
+console.log("--------------------------------------------")
+
+setTimeout(function ()
+{
+    console.log("Execute a função em 2 segundos")
+}, 3000)
